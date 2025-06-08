f61c934c82fb9e525404f19f13fe57a19816bafc
AminBadalzade <amin.badalzade@sa.stud.vu.lt>
2025-06-08 22:08:45 +0300

    Add project files.

---
+++ WordScanner/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+
+namespace ScannerAApp
+{
+    class ScannerA
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: ScannerA <directoryPath> <pipeName>");
+                return;
+            }
+
+
+
+            string directoryPath = args[0];
+            string pipeName = args[1];
+
+        }
+    }
+}
